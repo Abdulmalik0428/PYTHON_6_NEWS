0a2804d1bdbc79b054449a3cd628d0227f692530
Abdulmalik <abdumalik0428@gmail.com>
2025-08-13 23:59:33 +0500

    post,update,delate

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # PYTHON_6_NEWS
+News website written in Django
+
+HTTP methods :
+5
+GET -- Ma'lumot olish uchun
+POST -- Ma'lumot kiritish uchun
+PUT -- Ma'lumotni o'zgartirish uchun
+PETCH -- Ma'lumotni qisman o'zgartirish uchun
+DELATE -- Ma'lumotni o'chirish uchun
+
+
+CRUD operations
+Create
+Retrieve
+Update
+Delete
--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import redirect , render , get_object_or_404
+from django.urls import reverse
 
 from news.models import New , Category
 
@@ -16,8 +17,41 @@ def news_list_view(request):
 
 def news_detail_view(request , pk):
     news = get_object_or_404( New , id=pk )
+    categories = Category.objects.all()
     context = {
         'news':news ,
+        'categories':categories,
     }
+    if request.method == "POST":
+        news.category = Category.objects.get(id = request.POST.get('category'))
+        news.title = request.POST.get('title')
+        news.content = request.POST.get('content')
+        if request.FILES.get('image'):
+            news.image = request.FILES.get('image')
+        news.save()
 
     return render(request , 'news_detail.html' ,context)
+
+def add_news_view(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        date = {
+            'categories': categories
+        }
+        return render(request , 'add_news.html' , date)
+    elif request.method == "POST" :
+        category = Category.objects.get(id = request.POST.get('category'))
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        image = request.FILES.get('image')
+        news = New.objects.create(title=title,
+                                  category=category,
+                                  content=content,
+                                  image=image,
+                                  )
+        return redirect(reverse('news_detail', kwargs={'pk': news.id}))
+        
+def delete_news_view(request,pk):
+    news = New.objects.get(id=pk )
+    news.delete()
+    return render(request , 'news_deleted.html')
